7f75f4fa5806dfcbca2d6c54f8218c8ba01f8ca8
Marrrrrrccc <60645901+Marrrrrrccc@users.noreply.github.com>
2021-10-07 17:36:44 +0800

    Update MainMethod.py

--- MainMethod.py
+++ MainMethod.py
@@ -3,6 +3,7 @@ from requests import get
 
 loc = get('https://ipapi.co/json/')
 ip = loc.json()
+choice = input("What do you want to display?")
 
 print("My location is " + ip['city'] + ", " + ip['region'] +", "+ ip['country_name'] )
 print("My ip address is "+ ip['ip'])
