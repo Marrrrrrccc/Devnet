a5837674c016cee800b2ae85e6d7ee561e652666
Marrrrrrccc <60645901+Marrrrrrccc@users.noreply.github.com>
2021-10-07 17:33:27 +0800

    Update MainMethod.py

--- MainMethod.py
+++ MainMethod.py
@@ -1,7 +1,9 @@
+#need natin ng mga comments
 from requests import get
 
 loc = get('https://ipapi.co/json/')
 ip = loc.json()
+
 print("My location is " + ip['city'] + ", " + ip['region'] +", "+ ip['country_name'] )
 print("My ip address is "+ ip['ip'])
 print("My internet service provider is " + ip['org'])
