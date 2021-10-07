cf37c69347ca1046b71d386ae1c4d1bf4d5e23bd
Cyril Ken A. Verdad <verdad.cyrilken@gmail.com>
2021-10-07 19:38:19 +0800

    Update MainMethod.py

--- MainMethod.py
+++ MainMethod.py
@@ -6,24 +6,21 @@ while (True) :
     # loc = get('https://ipapi.co/json/')
     # print(loc.json())
     # ip = loc.json()
-    choice = input("""What do you want to display? \n
-                   1. Current Location \n
-                   2. Public Ip Address \n
-                   3. Service Provider \n""")
+    choice = input("\nWhat do you want to display? \n1. Current Location \n2. Public Ip Address \n3. Service Provider \n")
 
     # print("My location is " + ip['city'] + ", " + ip['region'] +", "+ ip['country_name'] )
     # print("My ip address is "+ ip['ip'])
     # print("My internet service provider is " + ip['org'])
 
     #Condition to end the program if the user enters "n" or "N"
-    end = input("Do you want to continue? (Y/N)")
+    end = input("\nDo you want to continue? (Y/N)")
     if (end.lower() == "n"):
         print("""\n\t========================================== 
         \tThis program was made by: \n 
-        \tMarc Ricson Ricafort, \n 
-        \tStanley Orong III, \n
-        \tand Cyril Ken Verdad \n
-        \tFrom 4-ITG \n
+        \t   Marc Ricson Ricafort, \n 
+        \t     Stanley Orong III, \n
+        \t   and Cyril Ken Verdad \n
+        \t        From 4-ITG \n
         ==========================================""")
         break
     else:
