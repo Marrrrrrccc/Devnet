ac3edcfc2e588fb137cd328012c02386b1bb550e
Cyril Ken A. Verdad <verdad.cyrilken@gmail.com>
2021-11-13 15:49:58 +0800

    fixed variables

--- gui.py
+++ gui.py
@@ -3,8 +3,36 @@ from tkinter import messagebox
 from tkinter.ttk import Label
 #importing get from requests module for API calls
 from requests import get
-loc = get('https://ipapi.co/json/') #Fetching data from the api
-ip = loc.json() #Formatting to json format to print it in the terminal
+
+#functions
+def getInformation(): #onclick function
+    loc = get('https://ipapi.co/json/') #Fetching data from the api
+    ip = loc.json() #Formatting to json format
+    #referencing global variables
+    global currentLoc
+    global currentLoc
+    global publicIP
+    global serviceProvider
+    global currency
+    global languages
+    global asn
+    global timezone
+    global zipCode
+    global capital
+    global population
+    #setting variable information
+    currentLoc = ip['city'] + ", " + ip['region'] + ", " + ip['country_name']
+    publicIP = ip['ip']
+    serviceProvider = ip['org']
+    currency = ip['currency_name']
+    languages = ip['languages']
+    asn = ip['asn']
+    timezone = ip['timezone']
+    zipCode = ip['postal']
+    capital = ip['country_capital']
+    population = ip['country_population']
+        
+
 
 #main 
 window = Tk()
@@ -12,111 +40,71 @@ window.title('IPGetR')
 header = StringVar() #message textvariable
 choices = StringVar()
 window.resizable(0,0) #Sets the window to unresizable
-window.geometry('550x600') #Adds a fixed width and height
+window.geometry('570x600') #Adds a fixed width and height
 innerWindow= Frame(window, relief= 'sunken') #Frame used to add padding between window and text
-innerWindow.pack(fill= BOTH, expand= True, padx= 10, pady=20)
-#funtions
-def click(): #onclick function
-    enteredtext = textin.get()
-    enteredtext1 = ipAddress.get()
-    # if enteredtext1 is not empty then use it in the api call
-    # error to be fixed
-    # nakukuha naman ip address
-    #pero may error
-    # if enteredtext1:
-    #     url = 'https://ipapi.co/' + enteredtext1 + '/json/'
-    #     loc = get(url)
-    #     ip1=loc.json()
-    #     #print all the necessary data from the api call
-    #     var.set(ip1['region'])
-    #                       # text="\nYour location is  " + ip1['city'] + ", " + ip1['region'] + ", " + ip1['country_name']
-    #                       #      + "\nYour ip address is " + ip1['ip']
-    #                       #      + "\nYour internet service provider is " + ip1['org']
-    #                       #      + '\nYour country\'s currency is ' + ip1['currency_name']
-    #                       #      + '\nYour country\'s languages is ' + ip1['languages']
-    #                       #      + "\nYour autonomous system number is " + ip1['asn']
-    #                       #      + "\nYour country\'s Timezone is " + ip1['timezone']
-    #                       #      + "\nYour zip code is " + ip1['postal']
-    #                       #      + "\nYour country\'s capital is " + ip1['country_capital']
-    #                       #      + "\nYour country\'s population is {}".format(ip1['country_population']))
-    if enteredtext == '1':
-        messagebox.showinfo("IPGetR", "\nYour location is  " + ip['city'] + ", " + ip['region'] + ", " + ip['country_name'])
-    elif enteredtext == '2':
-        messagebox.showinfo("IPGetR", "\nYour ip address is " + ip['ip'])
-    elif enteredtext == '3':
-        messagebox.showinfo("IPGetR", "\nYour internet service provider is " + ip['org'])
-    elif enteredtext == '4':
-        messagebox.showinfo("IPGetR", '\nYour country\'s currency is ' + ip['currency_name'])
-    elif enteredtext == '5':
-        messagebox.showinfo("IPGetR", '\nYour country\'s languages is ' + ip['languages'])
-    elif enteredtext == '6':
-        messagebox.showinfo("IPGetR", "\nYour autonomous system number is " + ip['asn'])
-    elif enteredtext == '7':
-        messagebox.showinfo("IPGetR", "\nYour country\'s Timezone is " + ip['timezone'])
-    elif enteredtext == '8':
-        messagebox.showinfo("IPGetR", "\nYour zip code is "+ ip['postal'])
-    elif enteredtext == '9':
-        messagebox.showinfo("IPGetR", "\nYour country\'s capital is " + ip['country_capital'])
-    elif enteredtext =='10':
-        messagebox.showinfo("IPGetR", "\nYour country\'s population is {}" .format(ip['country_population']))
-    elif enteredtext =='11':
-        messagebox.showinfo("IPGetR",  "\nYour location is  " + ip['city'] + ", " + ip['region'] + ", " + ip['country_name']
-            +"\nYour ip address is " + ip['ip']
-            +"\nYour internet service provider is " + ip['org']
-            +'\nYour country\'s currency is ' + ip['currency_name']
-            +'\nYour country\'s languages is ' + ip['languages']
-            +"\nYour autonomous system number is " + ip['asn']
-            +"\nYour country\'s Timezone is " + ip['timezone']
-            +"\nYour zip code is "+ ip['postal']
-            +"\nYour country\'s capital is " + ip['country_capital']
-            +"\nYour country\'s population is {}" .format(ip['country_population']))
-    else: 
-        messagebox.showerror('IPGetR',"=Please enter only a number from 1 to 11.")
-        
+innerWindow.pack(fill= BOTH, expand= True, padx= 75, pady=20)
 
-def placeholder(event,text):
-    text.configure(state='normal')
-    text.delete('0', END)
+#variables
+currentLoc = ""
+publicIP = ""
+serviceProvider = ""
+currency = ""
+languages = ""
+asn = ""
+timezone = ""
+zipCode = ""
+capital = ""
+population = ""
 
+#Method to get initial information
+getInformation()
 
 #label/ message (static)
-# Message1 = Message(window, textvariable = header, width = 1000, ).pack()
+#Title
 Label(innerWindow, text = "Welcome to IPGetR", font = ("Helvetica bold", 25)).pack()
-Label(innerWindow, text = "The program where you can get information from your IP!", font = ("Helvetica", 14)).pack()
+Label(innerWindow, text = "The program where you can get information from your IP!  ", font = ("Helvetica", 14)).pack()
+Label(innerWindow, text='\n\nYour Information', font=("Helvetica bold", 14), anchor='w').pack(fill='both')
 
-Message2 = Message(innerWindow, textvariable = choices,  width= 1000).pack()
+#Content
+Label(innerWindow, text='Current Location: ' + currentLoc, font=("Helvetica", 14), anchor='w').pack(fill='both')
+Label(innerWindow, text='Public IP Address: ' + publicIP, font=("Helvetica", 14), anchor='w').pack(fill='both')
+Label(innerWindow, text='Internet Service Provider (ISP): ' + serviceProvider, font=("Helvetica", 14), anchor='w').pack(fill='both')
+Label(innerWindow, text='Currency: ' + currency, font=("Helvetica", 14), anchor='w').pack(fill='both')
+Label(innerWindow, text='Languages: ' + languages, font=("Helvetica", 14), anchor='w').pack(fill='both')
+Label(innerWindow, text='Autonomous System Number: ' + asn, font=("Helvetica", 14), anchor='w').pack(fill='both')
+Label(innerWindow, text='Timezone: ' + timezone, font=("Helvetica", 14), anchor='w').pack(fill='both')
+Label(innerWindow, text='Zip Code: ' + zipCode, font=("Helvetica", 14), anchor='w').pack(fill='both')
+Label(innerWindow, text="Country's Capital: " + capital, font=("Helvetica", 14), anchor='w').pack(fill='both')
+Label(innerWindow, text="Countery's Population: " + population, font=("Helvetica", 14), anchor='w').pack(fill='both')
 
 
-
-
-choices.set("\nWhat do you want to display? \n"
-                   "1. Current Location \n"
-                   "2. Public Ip Address \n"
-                   "3. Internet Service Provider \n"
-                   "4. Currency\n"
-                   "5. Languages\n"
-                   "6. Autonomous system number\n"
-                   "7. Timezone\n"
-                   "8. Zip code\n"
-                   "9. Country capital\n"
-                   "10. Country Population\n"
-                   "11. Display all of the above\n")
-#input
-textin = Entry(window, width= 30)
-textin.insert(0, "Enter a number from 1 to 11")
-textin.configure(state='disabled')
-textin.bind('<Button-1>', lambda e: placeholder(e,textin))
-textin.pack()
-
-#placeholder
-ipAddress = Entry(window, width= 30)
-ipAddress.insert(0, "Enter your ip address")
-ipAddress.configure(state='disabled')
-ipAddress.bind('<Button-1>', lambda e: placeholder(e,ipAddress))
-ipAddress.pack()
+# #input
+# textin = Entry(window, width= 30)
+# textin.insert(0, "Enter a number from 1 to 11")
+# textin.configure(state='disabled')
+# textin.bind('<Button-1>', lambda e: placeholder(e,textin))
+# textin.pack()
+# choices.set("\nWhat do you want to display? \n"
+#                    "1. Current Location \n"
+#                    "2. Public Ip Address \n"
+#                    "3. Internet Service Provider \n"
+#                    "4. Currency\n"
+#                    "5. Languages\n"
+#                    "6. Autonomous system number\n"
+#                    "7. Timezone\n"
+#                    "8. Zip code\n"
+#                    "9. Country capital\n"
+#                    "10. Country Population\n"
+#                    "11. Display all of the above\n")
+# #placeholder
+# ipAddress = Entry(window, width= 30)
+# ipAddress.insert(0, "Enter your ip address")
+# ipAddress.configure(state='disabled')
+# ipAddress.bind('<Button-1>', lambda e: placeholder(e,ipAddress))
+# ipAddress.pack()
 
 #button 
-submit = Button(window, text= "submit", width = 7 , command = click)
-submit.pack()
+# submit = Button(window, text= "submit", width = 7 , command = click)
+# submit.pack()
 
 window.mainloop()
